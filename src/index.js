a5d2f47c3bd917b5a4ab79954ce17f31f0daf3f6
Yassine <progyss9@gmail.com>
2023-03-16 12:19:36 +0100

    Add form popup to add tasks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import { addProject, addTask } from './dom';
 
+function toggleForm() {
+  const form = document.getElementById('form');
+  form.classList.toggle('show');
+}
+
 const project = document.getElementById('add-project');
 project.addEventListener('click', addProject);
 
 const task = document.getElementById('add-task');
-task.addEventListener('click', addTask);
+task.addEventListener('click', () => {
+  task.classList.add('hide');
+  toggleForm();
+});
+
+const addTaskBtn = document.getElementById('addTask');
+addTaskBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  addTask();
+});
+const cancelTaskBtn = document.getElementById('cancelTask');
+cancelTaskBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  task.classList.remove('hide');
+  toggleForm();
+});
